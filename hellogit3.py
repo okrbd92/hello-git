c3b068236b659ccf3f16c883e6c8e1d343bf6358
Oskar R Barrera <108951942+okrbd92@users.noreply.github.com>
2024-05-26 13:41:49 -0400

    Este es mi pequeno detalle

--- hellogit3.py
+++ hellogit3.py
@@ -1,6 +1,3 @@
 
-<<<<<<< HEAD
-print ("este es un tercero intento login")
-=======
-print ("este es un tercero intento del proyecto dado")
->>>>>>> main
+
+print ("este es un tercero intento login gracias")
